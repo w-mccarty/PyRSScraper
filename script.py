c987a81c3faa042bd9b858cfa41004555bf561ae
w-mccarty <94848975+w-mccarty@users.noreply.github.com>
2024-07-18 17:34:32 -0400

    Update script.py

--- script.py
+++ script.py
@@ -9,90 +9,75 @@ from datetime import datetime
 import pytz
 from unidecode import unidecode
 import eventlet
-######################################################
+from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
+
+################################ CUSTOMIZEE THESE AS NEEDED ###############################
+#timezone
+tzone = 'US/Eastern'
 #location of RSS html files
-htmlDir = "/var/www/html/home/usb/feeds/"
-#your timezone
-tZone = 'US/Eastern'
+htmlDir = "/var/www/html/rss/"
+
+#this script should be scheduled to run with cron hourly, use the scheduler to set the hours that each url is pulled
+#referenced in urls[x][0]  
+#ie: scheduler[0] = every day at 6am, scheduler[3] = hourly starting at 6am...
 #
-#use cron to schedule hourly
-scheduler = [ 						# referenced in urls[x][0] (line 32)
-'06', 							# 0 = daily
-'06,12', 						# 1 = twice daily
-'06,08,10,12,14,16,18,20,22', 				# 2 = every ~2 hours
-'06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23'	# 3 = hourly
+scheduler = [
+'06',
+'06,12',
+'06,08,10,12,14,16,18,20,22',
+'06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23'
 ]
-#
+
 #to find YOUTUBE channel ID RSS url
 #   1. Open channel, right click view source
 #   2. Ctrl + F "https://www.youtube.com/channel/" to find channel ID
 #   3. Add ID to end of "https://www.youtube.com/feeds/videos.xml?channel_id="
 #
-urls = [ #scheduler[x],name,url
+urls = [
 [3,'Hacker News Frontpage','https://hnrss.org/frontpage'],
 [3,'Reddit Technology','https://www.reddit.com/r/technology/hot.rss']
 ]
-######################################################
-timezone = pytz.timezone(tZone)
-localnow = datetime.now(timezone).strftime('%H') 	#Local timezone used for scheduling only
-dnow = str(datetime.now().strftime('%H')) 		#we assume RSS using +00:00 UTC timezone in the <updated> block of feed
-hnow = str(datetime.now().strftime('%m-%d')) 		#we assume RSS using +00:00 UTC timezone in the <updated> block of feed
-print(dnow + "(" + localnow + ") - " + hnow)
+############################################################################################
+
+timezone = pytz.timezone(tzone)
+localnow = datetime.now(timezone).strftime('%H')
 urllen = len(urls)
 urlrange = range(0,urllen)
 for x in urlrange:
 	if str(localnow) in (scheduler[(urls[x][0])]):
 		try:
+			fname = urls[x][1].replace(" ","")
+			rfile = str(htmlDir) + str(fname) + ".html"
 			with eventlet.timeout.Timeout(5):
 				response = requests.get(urls[x][2])
 			parseurl = str(urls[x][2])
 			d = feedparser.parse(parseurl)
 			dl = []
-			fname = urls[x][1].replace(" ","")
-			rfile = str(htmlDir) + str(fname) + ".html"
-			for post in d.entries:
-				v1hrcheck = post.updated_parsed.tm_hour
-				if v1hrcheck < (10):
-					v1hrstrip = ("0" + str(v1hrcheck))
+			for index, post in enumerate(d.entries, start = 0):
+				indexval = index
+				v1 = post.link
+				v2 = unidecode(post.title)
+				v3 = parse(post.published)
+				v3a = str(v3)[5:10] #mm-dd
+				v3b = str(v3).replace("-","").replace(" ","").replace(":","").replace("+","")
+				v4 = datetime.now().astimezone().replace(microsecond=0).isoformat()
+				v4 = parse(v4)
+				delta = relativedelta(v4 v3)
+				if ((delta.years < 0) or (delta.months < 0) or (delta.days < 0) or (delta.hours < 0)):
+					pnv = ["dfeed_fut","tfeed_fut"]
+				elif ((delta.years == 0) and (delta.months == 0) and (delta.days == 0) and (delta.hours < 2)):
+					pnv = ["dfeed_now","tfeed_now"]
+				elif ((delta.years == 0) and (delta.months == 0) and (delta.days == 0)):
+					pnv = ["dfeed_day","tfeed_day"]
 				else:
-					v1hrstrip = str(v1hrcheck)
-				v1mincheck = post.updated_parsed.tm_min
-				if v1mincheck < (10):
-					v1minstrip = ("0" + str(v1mincheck))
-				else:
-					v1minstrip = str(v1mincheck)
-				v1seccheck = post.updated_parsed.tm_sec
-				if v1seccheck < (10):
-					v1secstrip = ("0" + str(v1seccheck))
-				else:
-					v1secstrip = str(v1seccheck)
-				v1 = (v1hrstrip + "-" + v1minstrip + "-" + v1secstrip)
-				v2check = post.updated_parsed.tm_mon
-				if v2check < (10):
-					v2strip = ("0" + str(v2check))
-				else:
-					v2strip = str(v2check)
-				v2 = v2strip.lstrip("0")
-				v3check = post.updated_parsed.tm_mday
-				if v3check < (10):
-					v3strip = ("0" + str(v3check))
-				else:
-					v3strip = str(v3check)
-				if ((v2strip + "-" + v3strip) == (dnow)) and ((int(hnow) - int(v1hrstrip)) < 2):
-					newr = "rn"
-				else:
-					newr = "rl"
-				v3 = v3strip.lstrip("0")
-				v4 = post.link
-				v4titlePre = post.title
-				v4titlePost = unidecode(v4titlePre)
-				v5 = (v2strip + "-" + v3strip + "-" + v1)
-				v6 = ("<div id=\"" + v5 + "\" class=\"rssdiv\"><div class=\"rssdate\" id=\"" + newr + v2 + "-" + v3 + "\">" + v2strip + "-" + v3strip + " " + v1hrstrip + ":" + v1minstrip + "</div><div class=\"rsslink\" id=\"" + newr + v2 + "-" + v3 + "\"><a href=\"" + v4 + "\" target=\"_blank\">" + v4titlePost + "</a></div><br></div>")
-				dl.append(v6)
+					pnv = ["dfeed_old","tfeed_old"]
+				v5 = ("<div id=\"fdiv_" + str(x) + "_" + v3b + str(indexval) + "\" class=\"" + pnv[0] + " rssdiv\"><div class=\"" + pnv[1] + " rssdate\" id=\"rssdate_" + str(x) + "_" + str(indexval) + "\">" + v3a + "</div><div class=\"" + pnv[1] + " rsslink\" id=\"rsslink_" + str(x) + "_" + str(indexval) + "\"><a href=\"" + v1 + "\" target=\"_blank\">" + v2 + "</a></div><br></div>")
+				dl.append(v5)
 			dl2 = sorted(dl, reverse=True)
-			v7 = str(datetime.today().strftime('%H:%M,%m-%d-%Y'))
+			v6= str(datetime.today().strftime('%H:%M,%m-%d-%Y'))
 			with open(rfile, mode="wt", encoding="utf-8") as myfile:
-				myfile.write("<div id=\"rssheadlink\">" + str(urls[x][1]) + "</div><div id=\"rssheadupdate\">Last updated: " + v7 + "</div>")
+				myfile.write("<div id=\"rssheadlink\">" + str(urls[x][1]) + "</div><div id=\"rssheadupdate\">Last updated: " + v6 + "</div>")
 				myfile.write('\n'.join(dl2))
 		except requests.exceptions.ReadTimeout:
 			print("READ TIMED OUT -" + urls[x][2])
